78a3264ebed7f8c44e219aa4532f323460d77676
evindag <80323753+evindag@users.noreply.github.com>
2023-03-13 02:50:24 +0300

    commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Ferhat DAĞ
+ *
+ */
+module springIntro {
+}
---
+++ src/springIntro/OracleCustomerDal.java
@@ -0,0 +1,7 @@
+package springIntro;
+
+public class OracleCustomerDal implements ICustomerDal{
+	public void add() {
+		System.out.println("Added to Oracle");
+	}
+}
